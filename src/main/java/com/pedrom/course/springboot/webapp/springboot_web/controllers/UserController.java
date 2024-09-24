2567b82b24bc0149a8727ef425d6e16ee458de7e
Pedro Mereles <pedromereles83@gmail.com>
2024-09-23 22:08:32 -0400

    Ejemplo de Creacion de table y list

--- src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.pedrom.course.springboot.webapp.springboot_web.controllers;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 import com.pedrom.course.springboot.webapp.springboot_web.models.User;
 
 
@@ -14,12 +20,40 @@ public class UserController {
     public String details(Model model) {
 
         User user = new User( "Pedro", "Mereles");
+        user.setEmail("pedromereles83@gmail.com");
         model.addAttribute("title", "Hello World");
         model.addAttribute("user", user);
         //  model.addAttribute("name", "Pedro");
         //  model.addAttribute("lastName", "Mereles");
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+
+        /*List<User> users = Arrays.asList(
+             new User("juan", "perez"),
+             new User("pepe", "cardozo"),
+             new User("juan", "Aguilar", "licenciadoaguiar@cmal.com")
+             );*/
+      
+        model.addAttribute("title", "Listado usuarios");
+       // model.addAttribute("users", users);
+
+        return "list";
+
+    }
+
+    @ModelAttribute("users")
+    public List<User> userModel(){
+        List<User> users = Arrays.asList(
+        new User("juan", "perez"),
+        new User("pepe", "cardozo"),
+        new User("juan", "Aguilar", "licenciadoaguiar@cmal.com")
+        );
+
+        return users;
     }
     
 
--- src/main/java/com/pedrom/course/springboot/webapp/springboot_web/models/User.java
+++ src/main/java/com/pedrom/course/springboot/webapp/springboot_web/models/User.java
@@ -4,11 +4,20 @@ public class User {
 
     private String name;
     private String lastName;
+    private String email;
 
     
+    public User() {
+    }
+
     public User(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
+    }
+
+    public User(  String name, String lastName, String email ) {
+        this(name, lastName);
+        this.email = email;
     }
 
     public String getName() {
@@ -25,6 +34,14 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 
