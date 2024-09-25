ea868b14078e8a66468e141edc2919e408d7cc84
Pedro Mereles <pedromereles83@gmail.com>
2024-09-24 23:23:47 -0400

    Creacion metodo para uso de POST, utilizando insomnia

--- src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,11 +1,18 @@
 package com.pedrom.course.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pedrom.course.springboot.webapp.springboot_web.models.User;
 import com.pedrom.course.springboot.webapp.springboot_web.models.dto.ParamDto;
+
 
 
 
@@ -22,5 +29,25 @@ import com.pedrom.course.springboot.webapp.springboot_web.models.dto.ParamDto;
         param.setMessage(message);
         return param;
     }
+
+        //Rquest con mas de un path
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar( @PathVariable String product, @PathVariable Long id) {
+        
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product );        
+        json.put("id", id);
+        return json;
+    }
+    
+    
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        // se debe de realizar algun movmiento, puede ser un save en bd   
+        
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+    
     
 }
--- src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -54,7 +54,7 @@ public class RequestParamsController {
 
         return params;
     }
-    
-    
+
+
 
 }
