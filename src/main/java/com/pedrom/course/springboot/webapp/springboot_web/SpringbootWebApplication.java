57677944c3429ce1aed74b36763c1f829d407dd1
Pedro Mereles <pedromereles83@gmail.com>
2024-09-25 20:57:54 -0400

    Utilizacion de archivo properties

--- src/main/java/com/pedrom/course/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/pedrom/course/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//Se agrega en una clase ValuesConfig, para ser mas independiente
+/*@PropertySources(
+	{
+		@PropertySource("classpath:values.properties")
+	}
+)*/
+
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/pedrom/course/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.pedrom.course.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,21 @@ import com.pedrom.course.springboot.webapp.springboot_web.models.User;
 import com.pedrom.course.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 
-
-
 @RestController
 @RequestMapping("/api/var")
  public class PathVariableController {
 
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+    
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+    
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz( @PathVariable String message) {
@@ -48,6 +58,18 @@ import com.pedrom.course.springboot.webapp.springboot_web.models.dto.ParamDto;
         user.setName(user.getName().toUpperCase());
         return user;
     }
+
+    @GetMapping("/values")
+    public  Map<String, Object> values() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("message", message);
+        json.put("code", code);
+        json.put("username", username);
+        json.put("listOfValues", listOfValues);
+
+        return json;
+    }
+    
     
     
 }
